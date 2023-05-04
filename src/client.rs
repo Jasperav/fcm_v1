c80464a12e941699ab416edd39431b871775e7f4
jaspervisser <jasperav@hotmail.com>
2023-05-04 16:28:20 +0200

    Add batch

--- src/client.rs
+++ src/client.rs
@@ -89,7 +89,7 @@ impl Client {
                 validate_only: false,
                 message: &message,
             };
-            let json = serde_json::to_string(&wrapped).unwrap();
+            let json = serde_json::to_string(&wrapped).map_err(|| Error::Deserialization)?;
             let request = format!(
                 r#"
 --subrequest_boundary
